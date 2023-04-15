bb719a08b1a593ccfd8960a33ef03002edf3f4f2
csproul08 <Sproul-C@ulster.ac.uk>
2023-04-15 11:58:08 +0100

    Edited spilltime csv file and merged csv files
    Script added to save the names of columns in file properly, and to rename a column in the spilltime_all.csv to allow for merge of both csv files. Also dropped columns that are not needed

--- main.py
+++ main.py
@@ -9,21 +9,38 @@ spilltime = pd.read_excel(excel_file, sheet_name=None)
 # Combine data from .xlsx worksheets as single DataFrame
 spilltime_all = pd.concat(spilltime.values(), ignore_index=True)
 
-print(sewers.head())
-print(spilltime_all.head())
-
 # Change name of spilltime .xls column 'EA Permit Reference (EA Consents Database) to 'PERMIT_NUMBER'
+spilltime_all.columns = spilltime_all.columns.str.replace("EA Permit Reference (EA Consents Database)", "PERMIT_NUMBER")
 
-# Join OS Grid Ref CSV and Storm Overflow Annual Return .xls file based on column 'PERMIT_NUMBER'
+# save spilltime_all xlsx as a csv file for merge function
+spilltime_all.to_csv('spilltime_all.csv', encoding='utf-8')
 
+# Move the column name from the second row of the spilltime_all.csv file and set this as the column name, to allow the
+# merge function to take place for both csv files
+spilltime_all.columns = spilltime_all.iloc[0]
 
-# Remove columns no longer needed
-# Need to add columns from spillsite table
-df = sewers.drop(columns=['EFFLUENT_GRID_REF', 'DISCHARGE_SITE_TYPE_CODE', 'DISTRICT_COUNCIL', 'CATCHMENT_CODE', 'EA_REGION',
+#remove the second row as this is no longer needed
+spilltime_all = spilltime_all.drop(index=1)
+
+# Change the name of the column called 'EA Permit Reference\n(EA Consents Database)' to 'PERMIT_NUMBER' to allow for a merge of both csv files
+spilltime_all = spilltime_all.rename(columns={'EA Permit Reference\n(EA Consents Database)': 'PERMIT_NUMBER'})
+
+# Merge both csv files using 'PERMIT_NUMBER' as a common column
+sewers_spilltime = pd.merge(sewers, spilltime_all, on='PERMIT_NUMBER')
+
+# Remove columns no longer needed for later analysis
+sewers_spilltime = sewers_spilltime.drop(columns=['EFFLUENT_GRID_REF', 'DISCHARGE_SITE_TYPE_CODE', 'DISTRICT_COUNCIL', 'CATCHMENT_CODE', 'EA_REGION',
  'PERMIT_VERSION', 'RECEIVING_ENVIRON_TYPE_CODE', 'REC_ENV_CODE_DESCRIPTION', 'ISSUED_DATE',
 'EFFECTIVE_DATE', 'REVOCATION_DATE', 'STATUS_OF_PERMIT','STATUS_DESCRIPTION', 'OUTLET_NUMBER',
-                          'OUTLET_TYPE_CODE', 'OUTLET_GRID_REF', 'EFFLUENT_NUMBER'])
+'OUTLET_TYPE_CODE', 'OUTLET_GRID_REF', 'EFFLUENT_NUMBER', 'Water Company Name', 'Site Name\n(EA Consents Database)',
+'Site Name\n(WaSC operational)\n[optional]', 'WaSC Supplementary Permit Ref.\n[optional]',
+'Activity Reference on Permit', 'Storm Discharge Asset Type', 'Outlet Discharge NGR\n(EA Consents Database)',
+'WFD Waterbody ID (Cycle 2)\n(discharge outlet)', 'WFD Waterbody Catchment Name (Cycle 2)\n(discharge outlet)',
+'Receiving Water / Environment (common name)\n(EA Consents Database)',
+'Shellfish Water (only populate for storm overflow with a Shellfish Water EDM requirement)',
+'Treatment Method\n(over & above Storm Tank settlement / screening)', 'Initial EDM Commission Date',])
 
+print(sewers_spilltime.columns)
 
 # Save joined file into new csv ready to be loaded for folium interactive map
 
